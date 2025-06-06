067cec662d9aa90c8637aa35f9f377f21594f245
MathisBorger <mathisborger24@gmail.com>
2025-06-06 10:44:53 +0200

    raffichage page html

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 
 const app = express();
+const router = express.Router();
 
 app.get('/', (req, res) => (
     res.status(200).json({
