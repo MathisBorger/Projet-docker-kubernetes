654fcff30d1bd5f2f617eb3ce5b47032ce6f53db
MathisBorger <mathisborger24@gmail.com>
2025-06-10 14:53:36 +0200

    secret et configmap

--- app/backend/db/BackupDTB.bat
+++ app/backend/db/BackupDTB.bat
@@ -1,2 +1,13 @@
-powershell -Command docker exec -t projet-docker-kubernetes-db-1 pg_dumpall -c -U postgres > dump_DTB.sql
-exit
+@echo off
+setlocal
+
+for /f "tokens=1" %%i in ('kubectl get pods --no-headers ^| findstr "db-"') do (
+    set POD_NAME=%%i
+)
+echo Pod détecté : %POD_NAME%
+
+:: Effectue un dump de toutes les bases PostgreSQL et sauvegarde dans dump_DTB.sql
+kubectl exec -i %POD_NAME% -- pg_dumpall -c -U postgres > dump_DTB.sql
+
+echo ✅ Sauvegarde terminée : dump_DTB.sql
+pause
